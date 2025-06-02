e134d40f578e6440a503a822af1ec5f213c9dd73
SimonCROS <scros@student.42lyon.fr>
2025-06-02 18:08:41 +0200

    Start making request

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,8 +1,25 @@
 import {Button} from "react-native";
 import {useSession} from "@/hooks/useSession";
+import {useEffect} from "react";
 
 export default function Auth() {
-    const {signOut} = useSession();
+    const {session, signOut} = useSession();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('YOUR_API_URL', {
+                    headers: {
+                        'Authorization': `Bearer ${session!.accessToken}`
+                    }
+                });
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+    }, [session]);
 
     return (
         <Button title={"LogOut"} onPress={() => signOut()}></Button>
