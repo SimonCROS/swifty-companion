4f961ecd11c4b5bb0523671d1860150b5172ab1d
SimonCROS <scros@student.42lyon.fr>
2025-06-06 14:34:16 +0200

    Beautiful sign in page

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,14 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as AuthSession from 'expo-auth-session';
-import {Button} from 'react-native';
+import {ScrollView, View} from 'react-native';
 import {useSession} from "@/hooks/useSession";
 import {authDiscovery} from "@/api/auth";
-import {SafeAreaView} from "react-native-safe-area-context";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {Text} from "@/components/ui/text";
+import {Button} from "@/components/ui/button";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function SignIn() {
+    const insets = useSafeAreaInsets();
     const {signIn} = useSession();
     const [isSigningIn, setIsSigningIn] = useState(false);
 
@@ -34,14 +37,20 @@ export default function SignIn() {
     }, [signIn, response, isSigningIn]);
 
     return (
-        <SafeAreaView>
-            <Button
-                disabled={!request && !isSigningIn}
-                title="Index"
-                onPress={() => {
-                    promptAsync();
-                }}
-            />
-        </SafeAreaView>
+        <ScrollView contentContainerStyle={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingLeft: insets.left,
+            paddingRight: insets.right,
+        }}>
+            <View className='relative web:max-w-xs w-full h-full p-6 flex flex-col items-stretch justify-center'>
+                <Button onPress={() => promptAsync()} disabled={!request && !isSigningIn}>
+                    <Text>Sign In</Text>
+                </Button>
+            </View>
+        </ScrollView>
     );
 }
